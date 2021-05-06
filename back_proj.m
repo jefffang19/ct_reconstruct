c4983d82c8729648751659164ed15b978ba24c5c
jefffang19 <jefffang07@gmail.com>
2021-05-06 16:40:16 +0800

    add backprojection

---
+++ back_proj.m
@@ -0,0 +1,28 @@
+function img = back_proj(proj, degree)
+    [proj_len, num_angles] = size(proj);
+    
+    % frequency space
+    pad_len = sqrt(512^2+512^2);
+    reconstruct = zeros(cast(pad_len, 'int32'), cast(pad_len, 'int32'));
+    
+    % perform at each angle
+    for i=1:num_angles 
+        
+        backproj = zeros(cast(pad_len, 'int32'), cast(pad_len, 'int32'));
+        
+        % perform back projection
+        for j=1:proj_len
+            backproj(:, j) = backproj(:, j) + proj(:, i);
+        end
+
+        %  rotate
+        backproj = imrotate(backproj,(i-1)*degree,'bilinear','crop');
+        
+        reconstruct = reconstruct + backproj;
+    end
+    
+    reconstruct = reconstruct * pi / (2*num_angles);
+
+    img = reconstruct;
+
+end
--- part_two.m
+++ part_two.m
@@ -1,5 +1,8 @@
 img = read_img('resources/test.jpg');
-proj_collect = get_proj(img, 3);
+
+degree = 0.3;
+
+proj_collect = get_proj(img, degree);
 
 % plot projection image
 imshow(proj_collect,[],'InitialMagnification','fit')
@@ -9,33 +12,13 @@ colormap(gca,hot), colorbar
 
 filtered_proj = ramp_filter(proj_collect);
 
-% plo tfiltered projection image
+% plot filtered projection image
 imshow(filtered_proj,[],'InitialMagnification','fit')
 xlabel('\theta (degrees)')
 ylabel('x''')
 colormap(gca,hot), colorbar
 
-% tell matlab to do inverse FBP without a filter
-theta=0:3:180;
-I1 = iradon(filtered_proj, theta, 'linear', 'none');
-subplot(1,1,1);imagesc( real(I1) ); title('Manual filtering')
+% backprojection
+I = back_proj(filtered_proj, degree);
+subplot(1,1,1);imagesc( I ); title('backproject')
 colormap('gray'); axis image; axis off
-
-% frequency space
-pad_len = sqrt(512^2+512^2);
-freq_domain = zeros(cast(pad_len, 'int32'), cast(pad_len, 'int32'));
-mid = cast(pad_len/2, 'int32');
-for i=1:freq_i-1 
-    % draw a line in frequency space
-    freq_domain(mid, :) = real(fft(proj_collect(i, :)));
-    
-    
-    %  rotate
-    freq_domain = imrotate(freq_domain,i*degree,'bilinear','crop');
-end
-
-imshow(freq_domain)
-
-X = ifft2(freq_domain);
-
-imshow(real(X))
