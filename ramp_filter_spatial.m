25de8e51cd19fefad4f6ff15d1f22b27ead84afb
jefffang19 <jefffang07@gmail.com>
2021-05-11 17:50:35 +0800

    Change ift of ram-lak implementation

--- ramp_filter_spatial.m
+++ ramp_filter_spatial.m
@@ -10,13 +10,13 @@ function img = ramp_filter_spatial(proj)
     % h(n) = - 1 / (n^2 * pi^2) for n is odd
     % note that matlab indexing starts with 1 so n=0 is h[1] in matlab
     
-%     n = 0:proj_len/2;
-%  
-%     ramp_filter = zeros(1, proj_len/2); % h(n) = 0 for n is even
-%     ramp_filter(1) = 0.25;  % h(n) = 1/4 for n = 0
-%     ramp_filter(2:2:end) = - 1./(pi^2.*n(2:2:end).*n(2:2:end)); % h(n) = - 1 / (n^2 * pi^2) for n is odd
-% 
-%     ramp_filter = [ramp_filter(end:-1:2),ramp_filter];
+    n = 0:proj_len/2;
+ 
+    ramp_filter = zeros(1, proj_len/2); % h(n) = 0 for n is even
+    ramp_filter(1) = 0.25;  % h(n) = 1/4 for n = 0
+    ramp_filter(2:2:end) = - 1./(pi^2.*n(2:2:end).*n(2:2:end)); % h(n) = - 1 / (n^2 * pi^2) for n is odd
+
+    ramp_filter = [ramp_filter(end:-1:2),ramp_filter];
     
     % Method 2
     % Ram-Lak filter in freq doamin
@@ -25,8 +25,9 @@ function img = ramp_filter_spatial(proj)
     ramp_filt = abs(freqs);
 
     % inverse ft of H(v) to get Ram-Lak filter in spatial doamin
-    ramp_filt = real(ifftshift(ifft(ramp_filt)))./-2;
-    ramp_filt(363) = -ramp_filt(363);
+    ramp_filt = real(ifftshift(ifft(ifftshift(ramp_filt))))./2;
+    
+%     plot(1:724, ifftshift(ramp_filt))
     
     % filter in spatial domain
     filtered = zeros([proj_len, num_angles]);
