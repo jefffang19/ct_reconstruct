9256b26fb709e552072dd3cdb7b2aa0fc3aa8d3e
jefffang19 <jefffang07@gmail.com>
2021-05-12 09:35:30 +0800

    Add calculate reconstructin error

--- part_one.m
+++ part_one.m
@@ -1,30 +1,36 @@
 img = imread('resources/test.jpg');
 gray = rgb2gray(img);
 gray = imresize(gray, [512, 512]); % resize
-imshow(gray)
+gray = cast(gray, 'double');
 
 % Radon Transform
 degree = 3;
 theta = 0:degree:180;
 [R,xp] = radon(gray,theta);
 
-imshow(R,[],'Xdata',theta,'Ydata',xp,'InitialMagnification','fit')
-xlabel('\theta (degrees)')
-ylabel('x''')
-colormap(gca,hot), colorbar
-
 % iRadon Transform
 
 % Perform filtered backprojection.
 I1 = iradon(R,theta);
+I1 = imcrop(I1, [1 1 511 511]);
 % Perform unfiltered backprojection.
 I2 = iradon(R,theta,'linear','none');
+I2 = imcrop(I2, [1 1 511 511]);
+% normalize unfiltered backprojection
+I2 = I2./(max(I2, [], 'all') - min(I2, [], 'all')).*255;
 % Display the reconstructed images.
 
 figure
-subplot(1,2,1)
+subplot(1,3,1)
+imshow(gray,[])
+title('Origin Image')
+subplot(1,3,2)
 imshow(I1,[])
 title('Filtered Backprojection')
-subplot(1,2,2)
+subplot(1,3,3)
 imshow(I2,[])
 title('Unfiltered Backprojection')
+
+% caluculate reconstructed error
+filtered_error = sum(abs(I1 - gray), 'all');
+unfiltered_error = sum(abs(I2 - gray), 'all');
